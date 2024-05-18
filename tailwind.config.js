c55e09395ef366fe4f4e6c4b590d21e7aebc18c5
milad <mohammadhajiabbasi1997@gmail.com>
2024-05-18 16:45:52 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      extend: {
+        fontFamily: {
+          sans:["Vazir"],
+        },
+      },
+    },
+    plugins: [],
+  }
